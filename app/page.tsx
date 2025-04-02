4f397e9c77daef4e4f73075d424a09b4b8d72f34
Daniel Sousa <daniel.lgdn@gmail.com>
2025-04-02 15:59:53 -0300

    feat: add header componets and menu

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { SearchIcon } from "lucide-react"
 import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
 const Home = () => {
   return (
@@ -11,13 +12,23 @@ const Home = () => {
         <h2 className="text-xl font-bold">Olá, Daniel!</h2>
         <p> Segunda - Feira, 10 de Março.</p>
         <div className="mt-6 flex items-center gap-2">
-          <Input placeholder="Faça sua busca" />
-          <Button>
+          <Input
+            placeholder="Faça sua busca"
+            className="border-2 border-lime-900"
+          />
+          <Button className="cursor-pointer bg-lime-900 text-lime-200 hover:bg-lime-700">
             <SearchIcon />
           </Button>
         </div>
 
-        <div className="relative h-[280px] w-full"></div>
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="agende seu horário"
+            src="/Teste01.jpg"
+            fill
+            className="h-40 w-400 rounded-xl object-cover"
+          />
+        </div>
       </div>
     </div>
   )
